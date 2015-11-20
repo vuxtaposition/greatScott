16ad248c4cdaced7bed907f6b67593afa3bc2072
Alan Leonard <alan@mail.com>
2015-11-20 01:08:45 +0000

    updated admin with validation in controllerPart II

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,13 +12,17 @@ class StaticPagesController < ApplicationController
    
     @users = User.all
     @orders = Order.all
-    
-      
+   
+    # restrict admin page even if user types it into the url
+    if current_user.email
         if current_user.email  != 'alan@mail.com'
          redirect_to root_path
+       else
+         
         end
-     
     end
+    end
+   
 
   def contact
   end
